1e985d75d65b09ee4fa1282f92edb773ef4d4ef8
Somashekar B Konnur <somuenc@gmail.com>
2018-04-11 21:19:59 -0500

    routing-demo in angular 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentsComponent } from './departments/departments.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'departments', component: DepartmentsComponent },
+  { path: 'employees', component: EmployeeListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ DepartmentsComponent, EmployeeListComponent ];
